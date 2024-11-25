a6eb4d5f44673a1a6de8333657083329cb79bcb0
jeremyUnfv <jeremy.amazing.it@gmail.com>
2024-11-24 23:38:41 -0500

    carpetas y menus

---
+++ COMPONENTES_VISTA/navbar_admin.php
@@ -0,0 +1,20 @@
+<!-- Navbar Superior -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">ASOCIACION HI-COSAN</a>
+        
+        <div class="ms-auto">
+            <div class="dropdown">
+                <button class="btn btn-dark dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="bi bi-person-circle me-2"></i>
+                    Administrador
+                </button>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
+                    <li><a class="dropdown-item" href="#">Configuración</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><button class="dropdown-item text-danger" onclick="handleLogout()">Salir</button></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
---
+++ COMPONENTES_VISTA/sidebar_admin.php
@@ -0,0 +1,58 @@
+<!-- Sidebar -->
+<div class="sidebar" id="sidebar">
+    <!-- CORE -->
+    <div class="sidebar-section">
+        <div class="sidebar-heading">CORE</div>
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link active" href="#" data-bs-toggle="tooltip" data-bs-placement="right" title="Dashboard">
+                    <i class="bi bi-speedometer2 me-2"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+
+    <!-- INTERFACE -->
+    <div class="sidebar-section">
+        <div class="sidebar-heading">INTERFACE</div>
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link" href="#" data-bs-toggle="tooltip" data-bs-placement="right" title="Layouts">
+                    <i class="bi bi-grid me-2"></i>
+                    <span>Layouts</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" data-bs-toggle="tooltip" data-bs-placement="right" title="Pages">
+                    <i class="bi bi-file-text me-2"></i>
+                    <span>Pages</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+
+    <!-- ADDONS -->
+    <div class="sidebar-section">
+        <div class="sidebar-heading">ADDONS</div>
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link" href="#" data-bs-toggle="tooltip" data-bs-placement="right" title="Charts">
+                    <i class="bi bi-graph-up me-2"></i>
+                    <span>Charts</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" data-bs-toggle="tooltip" data-bs-placement="right" title="Tables">
+                    <i class="bi bi-table me-2"></i>
+                    <span>Tables</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
+
+<!-- Botón para colapsar/expandir el sidebar -->
+<button id="sidebarCollapse" class="btn" aria-label="Colapsar/Expandir sidebar">
+    <i class="bi bi-arrow-left-circle"></i>
+</button>
