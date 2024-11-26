58a1f9ef0f7b1fa7bfed8e552aaf2a6a31a0c20f
joelCahuanaP <cahuanaparedesjoel@gmail.com>
2024-11-25 19:30:59 -0500

    conexion

---
+++ BACKEND/CONEXION/conexion.php
@@ -0,0 +1,8 @@
+<?php
+  $host="localhost";
+  $nombre="entidad";
+  $usuario="root";
+  $contra="";  
+  $conexion=mysqli_connect($host,$usuario,$contra,$nombre)or die("Error de conexion") ;
+  
+?> 
