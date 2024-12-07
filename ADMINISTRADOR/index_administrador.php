3b5985cae7c59f099882a99dc09a9da1d75360ec
jeremyUnfv <jeremy.amazing.it@gmail.com>
2024-12-07 18:10:08 -0500

    nada

--- ADMINISTRADOR/index_administrador.php
+++ ADMINISTRADOR/index_administrador.php
@@ -6,20 +6,21 @@ if (isset($_SESSION['id'])) {
     // Si no hay sesión activa, redirige al login
     //header("Location: ../LOGIN/login.php");
     $user = $_SESSION["id"];
-   
-    $consulta= "SELECT username FROM usuarios WHERE username = '$user'";
-    $resultado= mysqli_query($conexion,$consulta);
-   
+
+    $consulta = "SELECT username FROM usuarios WHERE username = '$user'";
+    $resultado = mysqli_query($conexion, $consulta);
+
     $fila = mysqli_fetch_array($resultado);
     $uss  = $fila['username'];
-   
-    $texto = " | " ."Usuario: " .$uss;
-	$_SESSION['tex']=$texto;
+
+    $texto = " | " . "Usuario: " . $uss;
+    $_SESSION['tex'] = $texto;
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -28,6 +29,7 @@ if (isset($_SESSION['id'])) {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="../CSS/componentes_compartidos.css">
 </head>
+
 <body>
 
     <?php include '../COMPONENTES_COMPARTIDOS/navbar_admin.php'; ?>
@@ -40,21 +42,24 @@ if (isset($_SESSION['id'])) {
     <div class="main-content" id="mainContent">
         <h1>HOLA MUNDO</h1>
         <div class="sesion">
-       <ul>
-         <li><?php if(isset($texto)){
-			   echo "<html>";
-			   echo "<a class='boton' href='form_cambiar_contra.php' >Cerrar Sesion</a>";
-			   echo "</html>";
-		      }else{
-				  echo "<html>";
-				  echo "<a class='boton' href='login/menuregistro.php' >Iniciar Sesion</a>";
-				  echo "</html>";
-			  }  ?></li>
-		 <li><?php if(isset($texto)) {echo $texto;} ?></li>
-       </ul>
-     </div>
+            <ul>
+                <li><?php if (isset($texto)) {
+                        echo "<html>";
+                        echo "<a class='boton' href='form_cambiar_contra.php' >Cerrar Sesion</a>";
+                        echo "</html>";
+                    } else {
+                        echo "<html>";
+                        echo "<a class='boton' href='login/menuregistro.php' >Iniciar Sesion</a>";
+                        echo "</html>";
+                    }  ?></li>
+                <li><?php if (isset($texto)) {
+                        echo $texto;
+                    } ?></li>
+            </ul>
+        </div>
         <!-- Aquí va el contenido de tu dashboard -->
     </div>
-   <!--  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>-->
+    <!--  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>-->
 </body>
+
 </html>
